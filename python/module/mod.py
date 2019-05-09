7829bc4878bc258855ca4a4e3243a9265a5a4df7
李文胜 <liwensheng001@lianjia.com>
2019-05-09 19:34:23 +0800

    py

--- python/module/mod.py
+++ python/module/mod.py
@@ -8,6 +8,7 @@ import pacage
 # 相对导入根据当前__name__ 相对查找
 # 作为执行中的包,__name__永远是__main__ 故不可以使用相对路径
 # 作为被导的包,可以使用相对路径
+# 包大全https://docs.python.org/zh-cn/3.6/py-modindex.html
 time = __import__('time')
 moda.a()
 modb.b()
