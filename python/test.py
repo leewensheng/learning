0d615c8fd1fd0755419a317d2c2c3ffd6056b0d4
李文胜 <liwensheng001@lianjia.com>
2019-05-09 10:29:47 +0800

    module

--- python/test.py
+++ python/test.py
@@ -1,6 +1,18 @@
+import functools
+from inspect import itertools
+
 aaa = 3
 bbb = 4
 c = {aaa,bbb, aaa}
 e , f = c
 print(e, f)
 print(type(c))
+
+def fn():
+  pass
+class MyClass:
+  @staticmethod
+  def a():
+    pass
+print(type(fn))
+print(type(MyClass.a))
